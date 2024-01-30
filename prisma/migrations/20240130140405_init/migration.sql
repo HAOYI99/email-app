9f291e479774eebf49ed9f70e3087bcebf018ee1
CHIEW HAO YI <haoyichiew@gmail.com>
2024-01-30 22:53:48 +0800

    re-init schema

--- prisma/migrations/20240130140405_init/migration.sql
+++ prisma/migrations/20240130140405_init/migration.sql
@@ -1,8 +1,9 @@
 -- CreateTable
 CREATE TABLE "EmailRecord" (
     "id" SERIAL NOT NULL,
-    "sender" TEXT[],
+    "sender" TEXT NOT NULL,
     "receiver" TEXT[],
+    "subject" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "submitAt" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
